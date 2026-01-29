22e4254c31c461701a34a22becc83e7f1043328a
yashkhadasare-beep <yashkhadasare@gmail.com>
2026-01-30 04:00:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/track/0XRb6eZE4dBIJdqwf1Mixu?si=Dg1QEV0QSDSeBtq1tgiIrA&context=spotify%3Atrack%3A0XRb6eZE4dBIJdqwf1Mixu", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dwo5g2lrn/video/upload/v1769725651/SUNIYAN_20SUNIYAN_KoshalWorld.Com_gf3vyl.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
